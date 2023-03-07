0d63348f5470c0dc043a4300ae089a0dadd68e4c
Samu <58610736+samu841@users.noreply.github.com>
2023-03-07 22:05:28 +0000

    ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+
+        Scanner teclado = new Scanner(System.in);
+
+        // Implemente uma função que converta graus Celsius em Fahrenheit: F = (9/5) C + 32
+        /*double c, f;
+        System.out.print("\nIndique o valor em graus celsius: ");
+        c = teclado.nextDouble();
+        f = celsiusParaFahrenheit(c);
+        System.out.println(c + "ºC = " + f + "ºF");*/
+
+    }
+    /* 1
+     * @param c - graus celsius
+     * @return graus fahrenheit */
+    public static double celsiusParaFahrenheit(double c) {
+        return (9.0/5) * c + 32;
     }
 }
